014f69faf0ffca62e27883d72ab0c4c0cbbda80b
tehtnaz <jasebeaumont@gmail.com>
2026-07-06 15:28:21 -0400

    add spring lock enum

---
+++ Assets/Scripts/ems/SpringLockSide.cs
@@ -0,0 +1,9 @@
+// MICHAEL don't LEAVE ME HERE MICHAELLL ahahahwdahhh
+
+// which side of the spring has its position locked in place if spring is stretched
+public enum SpringLockSide
+{
+    LockLeft,
+    LockRight,
+    LockNone
+}
--- Assets/Scripts/src/Spring.cs
+++ Assets/Scripts/src/Spring.cs
@@ -1,14 +1,42 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Spring : MonoBehaviour
 {
     [SerializeField] float springConstant;
-    [SerializeField] float displacement; // positive displacement means being stretched, negative means compressed
+    float displacement; // positive displacement means being stretched, negative means compressed
     Rigidbody2D rigidbody2D;
+    SpriteRenderer spriteRenderer;
+    [SerializeField] SpringLockSide springLock = SpringLockSide.LockNone;
+
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetDisplacement(float newDisplacement)
+    {
+        // distance the centre of the object is displaced with new spring displacement, scaled with transform
+        float centreOffsetScaled = 0.5f * (newDisplacement - displacement) * transform.localScale.x;
+        switch (springLock)
+        {
+            case SpringLockSide.LockLeft:
+                transform.position += centreOffsetScaled * transform.right;
+            break;
+            case SpringLockSide.LockRight:
+                transform.position -= centreOffsetScaled * transform.right;
+            break;
+            // LockNone doesn't apply any change in position
+        }
+
+        displacement = newDisplacement;
+
+        // update visual stretch according to displacement
+        // increased scale decreases visual stretch 
+        spriteRenderer.size = new Vector2(1.0f + displacement / transform.localScale.x, 1.0f);
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -20,8 +48,7 @@ public class Spring : MonoBehaviour
 
             BallPhysics.BallPhysicsInstance.ApplyForce(springConstant * displacement * displacementDirection); 
 
-            rigidbody2D.MovePosition(rigidbody2D.position + displacementDirection * displacement); // TODO: delete this later
-            displacement = 0;
+            SetDisplacement(0);
         }
     }
 }
